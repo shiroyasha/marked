23afb02a26f990815e6913e219889e0defb80322
Igor Šarčević <igor@renderedtext.com>
2016-01-10 13:21:23 +0100

    Generate elixir tests from commonmark test cases

--- test/marked_test.exs
+++ test/marked_test.exs
@@ -2,7 +2,22 @@ defmodule MarkedTest do
   use ExUnit.Case
   doctest Marked
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  @common_mark_tests_path "test/common_mark_tests.json"
+
+  @common_mark_tests_path
+  |> File.read!
+  |> Poison.decode!
+  |> Enum.map fn(test_case) ->
+
+    test_title = "#{test_case["section"]} -- example #{test_case["example"]}"
+
+    test test_title do
+      markdown = unquote(test_case["markdown"])
+      html = unquote(test_case["html"])
+
+      assert Marked.to_html(markdown) == html
+    end
+
   end
+
 end
