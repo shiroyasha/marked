10585f0ca9d8c75cde94c6508302d6c9a269d1a3
Igor Šarčević <igor@renderedtext.com>
2016-01-18 19:48:58 +0100

    Introduce Marked.Block

--- lib/marked.ex
+++ lib/marked.ex
@@ -1,63 +1,10 @@
 defmodule Marked do
+
   def to_html(markdown) do
-    markdown |> lines |> tokenize([]) |> parse
-  end
-
-  defp lines(string) do
-    String.split(string, "\n")
-  end
-
-  defp parse([]) do
-    ""
-  end
-
-  defp parse(tokens = [%{type: :thematic_break, content: content} | rest]) do
-    Marked.Html.hr(content) <> parse(rest)
-  end
-
-  defp parse(tokens = [%{type: :simple, content: content} | rest]) do
-    paragraph = tokens
-                |> Enum.take_while(fn(token) -> token.type == :simple end)
-                |> Enum.map(fn(token) -> token.content end)
-                |> Enum.join("\n")
-                |> Marked.Html.paragraph
-
-    new_rest = tokens
-               |> Enum.drop_while(fn(token) -> token.type == :simple end)
-
-    paragraph <> parse(new_rest)
-  end
-
-  defp parse(tokens = [%{type: :list_item, content: content} | rest]) do
-    list_items = tokens
-                  |> Enum.take_while(fn(token) -> token.type == :list_item end)
-                  |> Enum.map(fn(token) -> String.replace(token.content, "- ", "") end)
-
-    new_rest = tokens |> Enum.drop_while(fn(token) -> token.type == :list_item end)
-
-    Marked.Html.ul(list_items) <> parse(new_rest)
-  end
-
-  defp parse(tokens = [%{type: :empty, content: content} | rest]) do
-    parse(rest)
-  end
-
-  defp parse([%{type: :atx_heading, level: level, content: content} | rest]) do
-    Marked.Html.header(content, level) <> parse(rest)
-  end
-
-  defp parse(_) do
-    ""
-  end
-
-  # tokens
-
-  defp tokenize([], result) do
-    result
-  end
-
-  defp tokenize([line | rest], result) do
-    tokenize(rest, result ++ [Marked.Line.parse(line)])
+    markdown
+    |> String.split("\n")
+    |> Enum.map(&Marked.Line.parse(&1))
+    |> Marked.Block.parse
   end
 
 end
---
+++ lib/marked/block.ex
@@ -0,0 +1,42 @@
+defmodule Marked.Block do
+
+  def parse(lines = [%{type: :thematic_break, content: content} | rest]) do
+    Marked.Html.hr(content) <> parse(rest)
+  end
+
+  def parse(lines = [%{type: :simple, content: content} | rest]) do
+    paragraph = lines
+                |> Enum.take_while(fn(token) -> token.type == :simple end)
+                |> Enum.map(fn(token) -> token.content end)
+                |> Enum.join("\n")
+                |> Marked.Html.paragraph
+
+    new_rest = lines
+               |> Enum.drop_while(fn(token) -> token.type == :simple end)
+
+    paragraph <> parse(new_rest)
+  end
+
+  def parse(lines = [%{type: :list_item, content: content} | rest]) do
+    list_items = lines
+                  |> Enum.take_while(fn(token) -> token.type == :list_item end)
+                  |> Enum.map(fn(token) -> String.replace(token.content, "- ", "") end)
+
+    new_rest = lines |> Enum.drop_while(fn(token) -> token.type == :list_item end)
+
+    Marked.Html.ul(list_items) <> parse(new_rest)
+  end
+
+  def parse(lines = [%{type: :empty, content: content} | rest]) do
+    parse(rest)
+  end
+
+  def parse([%{type: :atx_heading, level: level, content: content} | rest]) do
+    Marked.Html.header(content, level) <> parse(rest)
+  end
+
+  def parse(_) do
+    ""
+  end
+
+end
